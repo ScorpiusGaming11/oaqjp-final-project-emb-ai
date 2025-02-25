7c41ac88f76fb354c8bf4fa40e23d316e805828f
ScorpiusGaming11 <kishenthiren7@gmail.com>
2025-02-25 06:36:44 -0500

    Update return value of emotion_detector function

--- final_project/emotion_detection.py
+++ final_project/emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
   url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,4 +7,22 @@ def emotion_detector(text_to_analyze):
   input_obj = { "raw_document": { "text": text_to_analyze } }
 
   response = requests.post(url, json=input_obj, headers=header)
-  return response.text
+  formatted_response = json.loads(response.text)
+
+  anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+  disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+  fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+  joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+  sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+  emotion_scores = {
+    'anger': anger,
+    'disgust': disgust,
+    'fear': fear,
+    'joy': joy,
+    'sadness': sadness
+  }
+
+  dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+  
+  return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness, 'dominant_emotion': dominant_emotion}
